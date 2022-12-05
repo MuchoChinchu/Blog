5b2475ebb8c8480fe0146a3926f79eceff559d51
leonardo <cruzluisleonardo@gmail.com>
2022-12-05 18:28:23 -0300

    Se agrega index y post views. (clase 1)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
+
